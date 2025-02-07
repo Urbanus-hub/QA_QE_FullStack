defb2840e4e60666a0be55b97405f4ffb0a4e256
urbanus-hub <urbankollyk@gmail.com>
2025-02-07 14:41:57 +0300

    changed number

--- javascript/basics.js
+++ javascript/basics.js
@@ -3,6 +3,6 @@ let name = "kioko";
 console.log(typeof name);
 console.log(typeof undefined)
 console.log("5" + 2); //output: 52
- console.log("5" - 2); //output:3
+ console.log("5" - 3); //output:3
  
 console.log(5 + true);
