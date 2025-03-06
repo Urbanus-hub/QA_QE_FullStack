874be81b453c8f90229a7cfa4a3fc50fce7c96d7
urbanus-hub <urbankollyk@gmail.com>
2025-03-06 10:43:13 +0300

    express

--- javascript/1.basics/basics.js
+++ javascript/1.basics/basics.js
@@ -1,8 +1,29 @@
-console.log("hello world");
-let name = "kioko";
-console.log(typeof name);
-console.log(typeof undefined)
-console.log("5" + 2); //output: 52
- console.log("5" - 3); //output:3
- 
-console.log(5 + true);
+// console.log("hello world");
+// let name = "kioko";
+// console.log(typeof name);
+// console.log(typeof undefined)
+// console.log("5" + 2); //output: 52
+//  console.log("5" - 3); //output:3
+
+// console.log(5 + true);
+function findFirstNonRepeatedCharacter(str) {
+  let count = 0;
+  let unrepeated = "";
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (str.charAt(i) == str.charAt(j)) {
+        count++;
+      }
+    }
+    console.log(count);
+    if (count < 2) {
+      unrepeated = str.charAt(i);
+      return unrepeated;
+    } else {
+      count = 0;
+    }
+  }
+  return "";
+}
+
+console.log(findFirstNonRepeatedCharacter("roar"));
